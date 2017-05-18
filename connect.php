44e722738c1f8588daf30181df0721eb1191f330
Jose Quinones <jquinones618@gmail.com>
2017-05-18 17:54:43 -0400

    Uncommented include_once command

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     // Retrieve database connection variables
-//    include_once '../../../external_includes/wpgv.php'; REMOVE COMMENT FROM THIS LINE BEFORE UPLOADING TO LIVE SERVER
+    include_once '../../../external_includes/wpgv.php';
 
     // Open connection to database
 	$connect = mysqli_connect($servername, $user, $pwd, $db) or die("Sorry, not able to connect to database");
